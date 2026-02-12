864ad07dbcec966b4dd513844e5af86869e89e39
jack <khemrathseng@gmail.com>
2026-02-12 10:14:24 +0700

    First commit

---
+++ shared/src/commonMain/kotlin/com/example/shared/App.kt
@@ -0,0 +1,27 @@
+package com.example.shared
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun App(platformName: String) {
+    // You'll need to move your Theme here as well eventually!
+    Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+        Greeting(
+            name = platformName,
+            modifier = Modifier.padding(innerPadding)
+        )
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
